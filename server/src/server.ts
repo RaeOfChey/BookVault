af9815432eab7d2a8ab19fc1ace2bd8968be342e
RaeofChey <cheyennaraelynn@gmail.com>
2024-11-19 20:46:09 -0600

    X5

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,7 +29,7 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
     console.log('Serving static files');
 
     app.get('*', (_req, res) => {
